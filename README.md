1f2f0ae99cd7d98dcd6c7888540609c863221900
jesee030 <desiasd@qq,comgit config --global user.email desiasd@qq.com>
2022-05-06 13:08:58 +0800

    第六个项目——登陆界面2

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
-# Code05第六个项目——登陆界面
+# 第八个项目——登陆界面2
 
-# 实验目的
+## 实验目的
 
-- 掌握布局的组合使用方式；
-- 掌握Vector Asset资源的使用方式；
-- 掌握Image View、View等控件的使用方式；
+- 掌握SharedPreferences进行轻量级数据存储的方法；
 
-# 实验要求
+## 实验要求
 
-- 创建布局文件并对根据要求对布局进行设置；
+- 使用SharedPreferences保存用户登录信息；
+- 使用SharedPreferences读取用户登录信息，并填充相应控件；
 
-# 实验总结
+SharedPreferences所存储的xml文件存放于设备的/data/data/[package_name]/shared_prefs目录下，其中package_name为App的包名:
 
-1.Intent的使用:显式Intent,Intent的构造函数Intent(Context packageContext, Class<?> cls)
-，第一个参数Context要求提供一个启动活动的上下文，第二个参数Class则是指定想要启动的目标活动。
-
-``` java
-Intent intent=new Intent(LoginActivity.this,NaviActivity.class);
-startActivity(intent);
+``` xml
+<?xml version='1.0' encoding='utf-8' standalone='yes' ?>
+<map>
+    <boolean name="login_remember_password" value="true" />
+    <string name="login_password">123</string>
+    <string name="login_account_name">123</string>
+</map>
 ```
+
+# 效果
+
+![img.png](img.png)
 
 # 参考
 
-https://www.cnblogs.com/wanderingzj/p/4928460.html
-
-https://xxgqin.gitbook.io/android/ch02/ch02-3
+https://xxgqin.gitbook.io/android/ch04/ch04-1
--- app/src/main/java/com/example/code05/LoginActivity.java
+++ app/src/main/java/com/example/code05/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.code05;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -23,7 +27,42 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageView ivPwdSwitch = findViewById(R.id.iv_pwd_switch);
+
         etPwd = findViewById(R.id.et_pwd);
+        etAccount = findViewById(R.id.et_account);
+        cbRememberPwd = findViewById(R.id.cb_remember_pwd);
+        Button btLogin = findViewById(R.id.bt_login);
+        btLogin.setOnClickListener(this);
+
+        String spFileName = getResources()
+                .getString(R.string.shared_preferences_file_name);
+        String accountKey = getResources()
+                .getString(R.string.login_account_name);
+        String passwordKey = getResources()
+                .getString(R.string.login_password);
+        String rememberPasswordKey = getResources()
+                .getString(R.string.login_remember_password);
+
+
+        SharedPreferences spFile = getSharedPreferences(
+                spFileName,
+                MODE_PRIVATE);
+        String account = spFile.getString(accountKey, null);
+        String password = spFile.getString(passwordKey, null);
+        Boolean rememberPassword = spFile.getBoolean(
+                rememberPasswordKey,
+                false);
+
+        if (account != null && !TextUtils.isEmpty(account)) {
+            etAccount.setText(account);
+        }
+
+        if (password != null && !TextUtils.isEmpty(password)) {
+            etPwd.setText(password);
+        }
+
+        cbRememberPwd.setChecked(rememberPassword);
+
 
         ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
 
@@ -45,5 +84,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        String spFileName = getResources()
+                .getString(R.string.shared_preferences_file_name);
+        String accountKey = getResources()
+                .getString(R.string.login_account_name);
+        String passwordKey = getResources()
+                .getString(R.string.login_password);
+        String rememberPasswordKey = getResources()
+                .getString(R.string.login_remember_password);
+
+        SharedPreferences spFile = getSharedPreferences(
+                spFileName,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = spFile.edit();
+
+
+        if (cbRememberPwd.isChecked()) {
+            String password = etPwd.getText().toString();
+            String account = etAccount.getText().toString();
+
+            editor.putString(accountKey, account);
+            editor.putString(passwordKey, password);
+            editor.putBoolean(rememberPasswordKey, true);
+            editor.apply();
+        } else {
+            editor.remove(accountKey);
+            editor.remove(passwordKey);
+            editor.remove(rememberPasswordKey);
+            editor.apply();
+        }
     }
 }
